37944359abd61baead9d5ed264f2c1b927b02048
AnotherDev <joseluismanco37@gmail.com>
2024-07-13 13:22:59 +0300

    feat: implement worldcoin verification

--- packages/front-end/src/app/worldcoin/page.tsx
+++ packages/front-end/src/app/worldcoin/page.tsx
@@ -16,6 +16,20 @@ export default function WorldCoinPage() {
 
     const verifyProof = async (proof: ISuccessResult) => {
         try {
+            console.debug(
+                {
+                    args: [
+                        account.address!,
+                        BigInt(proof!.merkle_root),
+                        BigInt(proof!.nullifier_hash),
+                        decodeAbiParameters(
+                            parseAbiParameters('uint256[8]'),
+                            proof!.proof as `0x${string}`
+                        )[0],
+                    ],
+
+                }
+            )
             await writeContractAsync({
                 address: process.env.NEXT_PUBLIC_CONTRACT_ADDRESS as `0x${string}`,
                 account: account.address!,
@@ -48,8 +62,9 @@ export default function WorldCoinPage() {
                 app_id="app_staging_4989e6a8b385ae6116fb36aeae08c250"
                 action="verify-public-address"
                 verification_level={VerificationLevel.Orb}
-                handleVerify={verifyProof}
-                onSuccess={onSuccess}>
+                onSuccess={verifyProof}
+                signal={account.address}
+                >
                 {({ open }) => (
                     <button
                         onClick={open}
