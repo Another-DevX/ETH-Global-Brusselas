f52a2cf5b99bfbbd82011f43304202e30a312bb0
AnotherDev <joseluismanco37@gmail.com>
2024-07-13 04:02:47 +0300

    feat: add a way to connect with more people

--- packages/smart-contracts/src/ConnectionsManager.sol
+++ packages/smart-contracts/src/ConnectionsManager.sol
@@ -5,7 +5,8 @@ import {IWorldID} from "./interfaces/IWorldID.sol";
 
 contract ConnectionManager {
 
-    mapping(address => bool) private _isVerified;
+    mapping(address => bool) public _isVerified;
+    mapping(address => mapping(address => bool)) public connections;
 
     /// @dev This allows us to use our hashToField function on bytes
     using ByteHasher for bytes;
@@ -59,5 +60,10 @@ contract ConnectionManager {
         nullifierHashes[nullifierHash] = true;
 
         _isVerified[signal] = true;
+    }
+
+    function connect(address connector, address recipent) public {
+        require(!connections[connector][recipent], "The users are connected yet");
+        connections[connector][recipent] = true;
     }
 }
