c9a6140cb290abb1fd1e913853a0934917316f8b
AnotherDev <joseluismanco37@gmail.com>
2024-07-13 22:12:16 +0300

    feat: add apollo client provider

--- packages/front-end/src/app/providers.tsx
+++ packages/front-end/src/app/providers.tsx
@@ -6,6 +6,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { NextUIProvider } from '@nextui-org/react'
 import { DynamicContextProvider } from '@dynamic-labs/sdk-react-core'
 import { EthereumWalletConnectors } from '@dynamic-labs/ethereum'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
 
 
 const queryClient = new QueryClient()
@@ -19,25 +20,33 @@ export const config = createConfig({
 })
 
 function Providers({ children }: { children: React.ReactNode }) {
+    const client = new ApolloClient({
+        uri: 'https://api.studio.thegraph.com/query/83101/multi-plug/version/latest',
+        cache: new InMemoryCache()
+    });
 
 
 
     return (
         <DynamicContextProvider
-        settings={{
-          environmentId: "1950d097-0c06-4589-af21-f02e3f165c10",
-          walletConnectors: [EthereumWalletConnectors],
-        }}
-      >
-      
-            <QueryClientProvider client={queryClient}>
-                <WagmiProvider config={config}>
-                    <NextUIProvider>
-                        {children}
-                    </NextUIProvider>
-                </WagmiProvider>
-            </QueryClientProvider>
-      </DynamicContextProvider>
+            settings={{
+                environmentId: "1950d097-0c06-4589-af21-f02e3f165c10",
+                walletConnectors: [EthereumWalletConnectors],
+            }}
+        >
+            <ApolloProvider client={client}>
+
+
+                <QueryClientProvider client={queryClient}>
+                    <WagmiProvider config={config}>
+                        <NextUIProvider>
+                            {children}
+                        </NextUIProvider>
+                    </WagmiProvider>
+                </QueryClientProvider>
+            </ApolloProvider>
+
+        </DynamicContextProvider>
 
     )
 }
