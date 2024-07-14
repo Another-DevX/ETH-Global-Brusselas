50554244c35d99521f6e57c2c0cb2422717f4991
AnotherDev <joseluismanco37@gmail.com>
2024-07-14 07:05:18 +0300

    feat: add modal after connection

--- packages/front-end/src/app/connect/page.tsx
+++ packages/front-end/src/app/connect/page.tsx
@@ -2,19 +2,28 @@
 import React, { useEffect, useState } from 'react'
 import { useSearchParams } from 'next/navigation'
 import { useRouter } from 'next/navigation'
-import { Button, Spinner } from '@nextui-org/react'
+import { Button, Link, Spinner } from '@nextui-org/react'
 import axios from 'axios'
 import { createConfig, http, useAccount } from 'wagmi'
 import { DynamicConnectButton, useIsLoggedIn } from '@dynamic-labs/sdk-react-core'
 import type { Address } from 'viem'
 import { useEnsName } from 'wagmi'
 import { mainnet } from 'viem/chains'
+import {
+    Modal,
+    ModalContent,
+    ModalHeader,
+    ModalBody,
+    useDisclosure,
+    ModalFooter
+} from "@nextui-org/modal";
 
 function Page() {
     const searchParams = useSearchParams()
 
     const { push } = useRouter()
     const [isConnecting, setIsConnecting] = useState(false)
+    const [receipt, setReceipt] = useState('')
     const { address } = useAccount()
     const connector = searchParams.get('address')
     const isLoggedIn = useIsLoggedIn();
@@ -22,11 +31,13 @@ function Page() {
     const config = createConfig({
         chains: [mainnet],
         transports: {
-          [mainnet.id]: http('https://rpc.ankr.com/eth'),
+            [mainnet.id]: http('https://rpc.ankr.com/eth'),
         },
-      })
+    })
+    const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
-    
+
+
 
     const result = useEnsName({
         address: connector as Address,
@@ -48,11 +59,14 @@ function Page() {
                 recipient: connector
             })
             console.debug(response)
+            setReceipt(response.data)
+            onOpen()
             setIsConnecting(false)
         } catch (e) {
             console.error(e)
         }
     }
+
     if (!connector) return null
     if (result.isLoading) return <div className='min-h-screen flex justify-center items-center '>
 
@@ -62,7 +76,7 @@ function Page() {
     console.debug(result)
     return (
         <div className='min-h-screen flex justify-center items-center flex-col gap-4'>
-            <h2 className='text-lg font-semibold text-center'>Do you want to connect with {result.data ? result.data : `${connector.slice(0, 4)}...${connector.slice(-4)}`}?</h2>
+            <h2 className='text-lg font-semibold text-center'>Do you want to connect with {result.data ? result.data : `${connector.slice(0, 4)}...${connector.slice(-4)}`}? <br /> 🤝</h2>
 
             {
                 isLoggedIn ? <div className='flex gap-2'>
@@ -81,7 +95,27 @@ function Page() {
 
 
             }
-
+            <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+                <ModalContent>
+                    {(onClose) => (
+                        <>
+                            <ModalHeader className="flex flex-col gap-1">Connection succesful</ModalHeader>
+                            <ModalBody>
+                                <p>Congratulations, you've successfully connected with each other!</p>
+                            </ModalBody>
+                            <ModalFooter>
+                                <Button color="danger" variant="light" onPress={onClose}>
+                                    Close
+                                </Button>
+                                <Button href={`https://sepolia.basescan.org/tx/${receipt}`} as={Link}
+                                    showAnchorIcon color="primary" onPress={onClose}>
+                                    Go to txn
+                                </Button>
+                            </ModalFooter>
+                        </>
+                    )}
+                </ModalContent>
+            </Modal>
         </div>
     )
 }
